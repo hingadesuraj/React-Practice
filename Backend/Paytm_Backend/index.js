0c5fab70b3e0cf7b4cd19c25c45b5c221caf41f8
Suraj <hingadesuraj@gmail.com>
2024-03-01 18:13:32 +0530

    proper setup for use environment variable in express app

--- Backend/Paytm_Backend/index.js
+++ Backend/Paytm_Backend/index.js
@@ -1,3 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const app = express();
+const env = require('dotenv').config()
+
+ 
+
+app.get("/",(req,res)=>{
+    res.send("server is running").json({message:"server is running"}).status(200);
+})
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is runnig on port ${process.env.PORT}`)
+})
