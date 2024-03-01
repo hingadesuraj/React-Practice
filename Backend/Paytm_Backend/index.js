c078426e2128b58286ee32cd3e8c45a63683c648
Suraj <hingadesuraj@gmail.com>
2024-03-01 18:19:01 +0530

    use .env variable in index.js

--- Backend/Paytm_Backend/index.js
+++ Backend/Paytm_Backend/index.js
@@ -4,14 +4,14 @@ const app = express();
 const env = require('dotenv').config()
 
  
-
+ 
 app.get("/",(req,res)=>{
     res.send("server is running").json({message:"server is running"}).status(200);
 })
-
+ 
 
 
 
 app.listen(process.env.PORT,()=>{
-    console.log(`Server is runnig on port ${process.env.PORT}`)
-})
+    console.log(`Server is runnig on${process.env.PORT}`)
+}) 
